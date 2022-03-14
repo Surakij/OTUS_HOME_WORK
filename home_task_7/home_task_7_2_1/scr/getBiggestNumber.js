e193b57db3ff6619d688c8ca627c2db92f6f2667
Sergey <surakij6@gmail.com>
2022-03-14 17:58:47 +0400

    Added home task solution and tests for home task 7_2_1

---
+++ home_task_7/home_task_7_2_1/scr/getBiggestNumber.js
@@ -0,0 +1,5 @@
+function getBiggestNumber (a ,b) {
+    return (a > b)? a : b;
+}
+
+module.exports = getBiggestNumber;
---
+++ home_task_7/home_task_7_2_1/scr/getBiggestNumber.test.js
@@ -0,0 +1,11 @@
+const getBiggestNumber = require('./getBiggestNumber');
+
+test('Should shows biggest of two values', () => {
+    expect(getBiggestNumber(1, 2)).toBe(2)
+});
+test('Should shows biggest of two values', () => {
+    expect(getBiggestNumber(9, 1)).toBe(9)
+});
+test('Should shows biggest of two values', () => {
+    expect(getBiggestNumber(-5, 5)).toBe(5)
+});
