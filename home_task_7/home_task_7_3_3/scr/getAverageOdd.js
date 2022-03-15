d5c79a86ef6c0de3163a811f8f23927b7923a052
Sergey <surakij6@gmail.com>
2022-03-15 20:14:04 +0400

    added home task solution & test for getAverageOdd

---
+++ home_task_7/home_task_7_3_3/scr/getAverageOdd.js
@@ -0,0 +1,12 @@
+function getAverageOdd (number) {
+    let counter = 0;
+    let sumOdd = 0;
+    for (let i = 1; i <= number; i+=2) {
+
+        sumOdd += i;
+        counter ++;
+    }
+    return sumOdd/counter;
+}
+
+module.exports = getAverageOdd;
---
+++ home_task_7/home_task_7_3_3/scr/getAverageOdd.test.js
@@ -0,0 +1,11 @@
+const getAverageOdd = require('./getAverageOdd');
+
+test('Should return sum of average all odd numbers', ()=>{
+    expect(getAverageOdd(5)).toBe(3);
+});
+test('Should return sum of average all odd numbers', ()=>{
+    expect(getAverageOdd(10)).toBe(5);
+});
+test('Should return sum of average all odd numbers', ()=>{
+    expect(getAverageOdd(11)).toBe(6);
+});
